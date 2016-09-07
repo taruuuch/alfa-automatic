801b4045b37a0621001982f664701551e1ce1fb5
Artemchuk Dmytro <taruuuch@gmail.com>
2016-09-07 23:41:19 +0300

    Last big changes

---
+++ src/js/swiper-local.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper-container', {
+      nextButton: '.swiper-button-next',
+      prevButton: '.swiper-button-prev',
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true
+  })
+});
