873515369e467c6e508f9d03b556a97a3f4ce581
Artemchuk Dmytro <taruuuch@gmail.com>
2016-09-18 00:31:40 +0300

    Add map with point and scroll to id

--- src/js/backtotop.js
+++ src/js/backtotop.js
@@ -8,8 +8,14 @@ $(document).ready(function(){
 	});
 
 	$('.scrollToTop').click(function(){
-		$('html, body').animate({scrollTop : 0},800);
+		$('html, body').animate({scrollTop : 0},1000);
 		return false;
 	});
 
+	$("a").click(function () {
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top - 200;
+      $('html, body').animate({ scrollTop: destination }, 1000);
+      return false;
+    });
 });
