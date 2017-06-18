23cef5da9e8510e192d8bbd3e839bf6b5778a6d2
Artemchuk Dmytro <taruuuch@gmail.com>
2017-06-19 01:39:50 +0300

    Change js animate to main and update header

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("a").click(function() {
+    var elementClick = $(this).attr("href");
+    var destination = $(elementClick).offset().top - 200;
+    $('html, body').animate({
+      scrollTop: destination
+    }, 1000);
+    return false;
+  });
+
+  $(function() {
+    $(".watermark").watermark({
+      path: '../images/watermark.png'
+    });
+  });
+});
