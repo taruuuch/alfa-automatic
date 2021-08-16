fe40ccd9ba315021cbb1fb3d118a8eab0f05b3ea
taruuuch <taruuuch@gmail.com>
2021-08-16 14:55:28 +0300

    #update import

--- src/js/swiper-local.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  var swiper = new Swiper('.container', {
-      nextButton: '.button-next',
-      prevButton: '.button-prev',
-      slidesPerView: 1,
-      spaceBetween: 30,
-      loop: true
-  })
-});
